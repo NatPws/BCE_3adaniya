cf4bc731f5df4dadff5ec01aaa31679b1b851f33
Natalia <natalia.ab.ca@gmail.com>
2023-12-20 15:18:04 +0300

    поправила код в 7 задаче

--- 07/Program.cs
+++ 07/Program.cs
@@ -6,20 +6,23 @@
 //(1, 5, 13, 24, 7, 9), 8 -> Нет
 //(1, 3, 2, 17), 3 -> Да
 
-int number = 3;
-int[] array = new int[4] { 1, 3, 2, 17};
-
+int number = 9;
+int[] array = new int[6] { 1, 3, 2, 17, 18, 11 };
+bool flag = false;
 for (int i = 0; i < array.Length; i++)
 {
     if (number == array[i])
     {
-    Console.WriteLine("Да");
-    break;
-    }   
-    else
-    {
-    Console.WriteLine("Нет");
+        flag = true;
+        break;
     }
 }
 
-// да и нет - выдает по индексам массива, пока их перебирает
+if (flag)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
